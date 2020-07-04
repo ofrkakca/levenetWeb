de3e19bcab706b33aff480e8319c8a61355e3f6a
ofrkakca <faruk.akca@bil.omu.edu.tr>
2020-07-04 13:46:21 +0300

    Add project files.

---
+++ levenetWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace levenetWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
